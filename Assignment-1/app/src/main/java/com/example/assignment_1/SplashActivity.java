abd4714bfd6e3c931d98273f41b510a8fad448b0
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-09-05 14:58:09 +0500

    Added splashActivity java class

--- Assignment-1/app/src/main/java/com/example/assignment_1/SplashActivity.java
+++ Assignment-1/app/src/main/java/com/example/assignment_1/SplashActivity.java
@@ -1,4 +1,23 @@
+// SplashActivity.java
+
 package com.example.assignment_1;
 
-public class SplashActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        // Display splash screen for 3 seconds
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashActivity.this, SenderInfoActivity.class));
+            finish();
+        }, 3000);
+    }
 }
+
